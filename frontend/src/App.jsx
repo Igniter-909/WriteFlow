70c9de1318d4f22e2679d29b56b9f62837e8f19f
Igniter-909 <igniter909505@gmail.com>
2024-10-31 00:37:37 +0530

    added homepage ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes,Route } from "react-router-dom"
 import SignupPage from "./pages/auth/signup/SignupPage"
 import LoginPage from "./pages/auth/login/LoginPage"
+import HomePage from "./pages/home/HomePage"
 
 
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto'>
 			<Routes>
-				{/* <Route path='/' element={<HomePage />} /> */}
+				<Route path='/' element={<HomePage />} />
 				<Route path='/signup' element={<SignupPage />} />
 				<Route path='/login' element={<LoginPage />} />
 			</Routes>
