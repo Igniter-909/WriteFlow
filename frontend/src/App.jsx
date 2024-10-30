3a86d72c88e7f9d8c8ffd8de7d995b995139e8e1
Igniter-909 <igniter909505@gmail.com>
2024-10-31 01:04:39 +0530

    created the sidebars and createpost ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,20 @@ import { Routes,Route } from "react-router-dom"
 import SignupPage from "./pages/auth/signup/SignupPage"
 import LoginPage from "./pages/auth/login/LoginPage"
 import HomePage from "./pages/home/HomePage"
+import Sidebar from "./components/common/Sidebar"
+import RightPanel from "./components/common/RightPanel"
 
 
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto'>
-			<Routes>
+		<Sidebar />
+		<Routes>
 				<Route path='/' element={<HomePage />} />
 				<Route path='/signup' element={<SignupPage />} />
 				<Route path='/login' element={<LoginPage />} />
-			</Routes>
+		</Routes>
+		<RightPanel />
 		</div>
   )
 }
